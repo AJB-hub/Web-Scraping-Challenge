8653b3872d748411cabd8e33663d603d17f62175
andrew bly <andrewbly36@gmail.com>
2021-06-18 20:00:41 -0400

    finalized

--- MissionToMars/app.py
+++ MissionToMars/app.py
@@ -11,28 +11,35 @@ from flask import Flask, render_template
 from scrape import scrape
 from pushpull import get, push
 
-# Create an instance of our Flask app.
-app = Flask(__name__)
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.MarsData
+col = db["scrape_data"]
 
 #Scrape Data to start
-push()
+push(col)
+
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
 
 # Set route
 @app.route('/')
 def index():
 
-    return render_template('./html/index.html', 
-                                        title = get('p0')[0]['news_title'],
+    return render_template('index.html', 
+                                        title = get('p0', col)[0]['news_title'],
                            
-                                        para = get('p0')[0]['news_p'],
+                                        para = get('p0', col)[0]['news_p'],
 
-                                        img = get('p1')[0]['image_url'],
+                                        img = get('p1', col)[0]['image_url'],
 
-                                        table = get('p2')[0]['mars_table'],
+                                        table = get('p2', col)[0]['mars_table'],
 
-                                        h_title = [get(f'p{i}')[0]['title'] for i in range(3,7)],
+                                        h_title = [get(f'p{i}', col)[0]['title'] for i in range(3,7)],
 
-                                        h_url = [get(f'p{i}')[0]['img_url'] for i in range(3,7)],
+                                        h_url = [get(f'p{i}', col)[0]['img_url'] for i in range(3,7)],
 
                           )
 
@@ -42,24 +49,24 @@ def ScrapeRequest():
     
     try:
         
-        push()
+        push(col)
         
     finally:
         
-        return render_template('./html/index.html', 
-                                            title = get('p0')[0]['news_title'],
+        return render_template('index.html', 
+                                        title = get('p0', col)[0]['news_title'],
+                           
+                                        para = get('p0', col)[0]['news_p'],
 
-                                            para = get('p0')[0]['news_p'],
+                                        img = get('p1', col)[0]['image_url'],
 
-                                            img = get('p1')[0]['image_url'],
+                                        table = get('p2', col)[0]['mars_table'],
 
-                                            table = get('p2')[0]['mars_table'],
+                                        h_title = [get(f'p{i}', col)[0]['title'] for i in range(3,7)],
 
-                                            h_title = [get(f'p{i}')[0]['title'] for i in range(3,7)],
+                                        h_url = [get(f'p{i}', col)[0]['img_url'] for i in range(3,7)],
 
-                                            h_url = [get(f'p{i}')[0]['img_url'] for i in range(3,7)],
-
-                              )
+                          )
 
 if __name__ == "__main__":
     app.run(debug=True)
